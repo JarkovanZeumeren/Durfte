53842c0afc562c749b05ec2cc20bd3100de5f9d7
Jarko van Zeumeren <jzeumere0176@student.landstede.nl>
2023-02-04 16:37:05 +0100

    basics

--- Durfte-website/resources/views/welcome.blade.php
+++ Durfte-website/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jarko van Zeumeren</title>
+    <title>Durfte website</title>
     <link rel="icon" type="image/x-icon" href="/image/favicon (1).ico">
     <link rel="stylesheet" href="/css/poging2.css">
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
@@ -14,7 +14,9 @@
 
 <body class="body">
     <header class="header">
-
+            goeden dag
     </header>
+
+    <div>hoi</div>
 </body>
 </html> 
